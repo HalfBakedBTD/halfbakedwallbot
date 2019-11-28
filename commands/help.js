0c84a5d5a56933bc42216b2d5ed05eae714207cb
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2019-11-27 19:45:43 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (bot, message, args) => {
   let botEmbed = new Discord.RichEmbed()
   .setColor('#27ae60')
-  .setDescription("At least I work right?/n[invite](https://discordapp.com/oauth2/authorize?client_id=649394319148449795&permissions=8&scope=bot)");
+  .setDescription("At least I work right?");
   message.channel.send(botEmbed)
 }
 	      
