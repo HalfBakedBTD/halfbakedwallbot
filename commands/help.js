a217e921a72c9325717e45e25e78112f94c9c15d
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2019-11-27 20:10:04 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (bot, message, args) => {
   let botEmbed = new Discord.RichEmbed()
   .setColor('#27ae60')
-  .setDescription("At least I work right?");
+  .setDescription("Do `..clear` to mark the walls as cleared.");
   message.channel.send(botEmbed)
 }
 	      
